1a70095ac26f5f7877e06804dcb1762097d379c1
kevin lam <kevinlam07696@gmail.com>
2019-11-09 01:44:38 -0500

    Updating for Part2

--- app/src/main/java/com/example/flickster/DetailActivity.java
+++ app/src/main/java/com/example/flickster/DetailActivity.java
@@ -31,6 +31,7 @@ public class DetailActivity extends YouTubeBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
